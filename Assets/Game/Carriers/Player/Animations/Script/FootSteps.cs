4f69f11ea9a92e5121d8039d4c6d3d7d3555e510
Menachem shaphir (Black Out) <97192633+BBlack-Out@users.noreply.github.com>
2023-11-13 20:27:33 +0200

    Foot Steps

---
+++ Assets/Game/Carriers/Player/Animations/Script/FootSteps.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField] AudioClip[] footsteps;
+    AudioSource source;
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    public void PlaySoundRandom()
+    {
+        source.clip = footsteps[new System.Random().Next(footsteps.Length)];
+        source.Play();
+    }
+}
